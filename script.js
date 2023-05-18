6d9d02f42a7e367a4c998f6ce4a67ba221062678
seanhayes22 <36180761+seanhayes22@users.noreply.github.com>
2023-05-18 16:43:18 +1000

    changes

--- script.js
+++ script.js
@@ -6,10 +6,27 @@ function Disappear()
 function Reappear()
 {
     document.getElementById('mydiv').style.display = 'block';
-    var red = getRndInteger(0,255);
-    var blue = getRndInteger(0,255);
-    var green = getRndInteger(0,255);
+    let red = getRndInteger(0,255);
+    let blue = getRndInteger(0,255);
+    let green = getRndInteger(0,255);
     document.getElementById('mydiv').style.backgroundColor = 'rgb('+red+','+green+','+blue+')';
+}
+
+function newFunc()
+{
+    let pie = "";
+    let choice;
+
+    let userInput = prompt("What is your name?","");
+    while(choice = confirm("Are you"+pie+" sure?"))
+    {
+        pie += pie + " really";
+    }
+    if(!choice)
+    {
+        alert("I thought so.");
+    }
+
 }
 
 function getRndInteger(min, max) {
