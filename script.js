0665df26e95785efa290dbcdc3dfeaf6caa13f06
seanhayes22 <36180761+seanhayes22@users.noreply.github.com>
2023-05-29 11:04:11 +1000

    gg

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function Disappear()
+let red, green, blue;
+
+function myChange(colorVal)
 {
-    document.getElementById('mydiv').style.display = 'none';
+    let input1value = document.getElementById(colorVal).value;
+    document.getElementById(colorVal).style.backgroundColor = 'white';
+    if(!isNaN(input1value) && input1value < 256 && input1value >= 0)
+    {
+        switch(colorVal)
+        {
+            case "red":
+                red = input1value;
+                break;
+            case "green":
+                green = input1value;
+                break;
+            case "blue":
+                blue = input1value;
+                break;
+        }
+        console.log(red, green, blue, "vals");
+    }
+    else{
+        document.getElementById(colorVal).style.backgroundColor = 'red';
+    }
+    if(red != null && blue != null && green != null)
+    {
+        document.getElementById('mydiv').style.backgroundColor = "rgb("+red+","+green+","+blue+")";   
+    }
 }
 
 function Reappear()
@@ -14,11 +40,25 @@ function Reappear()
 
 function newFunc()
 {
-    let colour = prompt("What is your favourite colour?");
-    document.getElementById("mydiv").style.backgroundColor = colour;
-    alert("Like this?");
+    let colour = prompt("What is your name?");
+    //document.getElementById("mydiv").style.backgroundColor = colour;
+    alert("Hello " + colour);
 }
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function example()
+{
+    let redy = document.getElementById("red2").value;
+    let greeny = document.getElementById("green2").value;
+    let bluey = document.getElementById("blue2").value;
+    document.getElementById('mydiv').style.backgroundColor = 'rgb('+redy+','+greeny+','+bluey+')';
+}
+
+function daisy()
+{
+    let dandelion = document.getElementById('rose').value;
+    document.getElementById('mydiv').innerHTML = dandelion;
 }
