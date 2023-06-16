dd5468bfa5ff9932195876b9e269794fe587fddb
seanhayes22 <36180761+seanhayes22@users.noreply.github.com>
2023-06-16 12:56:47 +1000

    lt3

--- lt3.js
+++ lt3.js
@@ -7,40 +7,59 @@ function loadPage()
 {
     randomNum = getRndInteger(myMin,myMax);
     console.log(randomNum);
-    guessesLeft = 10;
-    document.getElementById("guesses").innerHTML = "Guesses Left: " + guessesLeft;
-    document.getElementById('theBody').style.backgroundColor = white;
 }
 
-function setMinMax()
+function resetGame()
 {
-    myMin = prompt("Set your minimum guess value", "1");
-    myMax = prompt("Set your minimum guess value", "100");
+    guessesLeft = Math.floor(myMax/10);
+    if(guessesLeft < 5)
+    {
+        guessesLeft = 5;
+    }
+    document.getElementById("guesses").innerHTML = "Guesses Left: " + guessesLeft;
+    document.getElementById('theBody').style.backgroundColor = "rgb(255, 255, 255)";
+    document.getElementById("myGuess").value = "";
     loadPage();
+}
+
+function setMax()
+{
+    myMax = prompt("Set your maximum guess value", "100");
+    document.getElementById("heading1").innerHTML = "Guess the number (1-"+myMax+")";
+    resetGame();
 }
 
 function guessNum()
 {
+    let myGuess = document.getElementById("myGuess").value;
+    if(isNaN(myGuess) || myGuess <= 0)
+    {
+        alert("Incorrect input");
+  //      console.log(`NaN: ${isNaN(myGuess)} \nLow guess ${myGuess<=0} \nHigh guess ${myGuess > myMax}`);
+//        console.log(myMax);
+        return;
+    }
     if(guessesLeft > 1)
     {
         guessesLeft--;
         document.getElementById("guesses").innerHTML = "Guesses Left: " + guessesLeft;
-        let myGuess = document.getElementById("myGuess").value;
-        if(!isNaN(myGuess) && myGuess == randomNum)
+        if(myGuess == randomNum)
         {
             alert("You got it!");
         }
-        let red = Math.abs(myGuess - randomNum)/100 * 255;
-        console.log(red);
-        let green = 255-red;
-        console.log(green);
+        // 2.0f * x, 2.0f * (1 - x), 0
+        let x = Math.abs(myGuess - randomNum)/myMax;
+        let red = 2 * x * 255;
+        //console.log(red);
+        let green = 2 * (1-x) * 255;
+        //console.log(green);
         document.getElementById("theBody").style.backgroundColor = `rgb(${red}, ${green}, 0)`;
         console.log(`rgb(${red}, ${green}, 0)`);
     }
     else
     {
-        alert("You ran out of guesses!")
-        loadPage();
+        alert("You ran out of guesses!\nIt was " + randomNum);
+        resetGame();
     }
 }
 
