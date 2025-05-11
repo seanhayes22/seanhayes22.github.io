b848c384f8e7304707dbe71e432ca6aae15f69d2
seanhayes22 <shay@tc.vic.edu.au>
2025-05-12 09:18:57 +1000

    chamges

--- Activity14/B/activity14.js
+++ Activity14/B/activity14.js
@@ -15,7 +15,10 @@ function toggle()
 
 function welcome()
 {
+    //console.log(-5 % 2)
+
     let myName = prompt('What is your name?')
+    
 
     if(myName == "Sean")
     // True
@@ -27,21 +30,32 @@ function welcome()
         document.getElementById('jennifer').innerText = "Hello, " + myName + " have a bad day";
     }
 
-
-
-
 }
 /////  TODAY ////////////////
 function birthdayColour()
 {
-    let day = document.getElementById('day').value / 31 * 255;
-    let month = document.getElementById('day').value / 12 * 255;
-    let year = (2025 - document.getElementById('day').value) / 100 * 255;
+    let day = document.getElementById('day').value, month, year
+    
+    if (day < 31)
+    {
+        day = document.getElementById('day').value / 31 * 255;
+        document.getElementById('day').style.backgroundColor = "white"
+    }
+    else
+    {
+        document.getElementById('day').style.backgroundColor = "red"
+    }
+    
+    
+    month = document.getElementById('month').value / 12 * 255;
+    year = (2025 - document.getElementById('year').value) / 100 * 255;
 
-
+    let red = day;
+    let green = month;
+    let blue = year;
 
     // This line does the same as the line underneath it
-    document.getElementById('jennifer').style.backgroundColor = "rgb(" + red + "," + blue + "," + green + ")";
+    // document.getElementById('jennifer').style.backgroundColor = "rgb(" + red + "," + blue + "," + green + ")";
     // This line does the same as the line above
-    document.getElementById('jennifer').style.backgroundColor = `rgb( ${red}, ${blue}, ${green})`
+    document.getElementById('jennifer').style.backgroundColor = `rgb( ${red}, ${green}, ${blue})`
 }
