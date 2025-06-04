41a6e489273f461639bfd3986e398fd442485630
seanhayes22 <shay@tc.vic.edu.au>
2025-06-05 09:00:26 +1000

    serf

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -1,3 +1,24 @@
-const newParagraph = document.createElement('p');
-newParagraph.innerHTML = "This wasn't here before";
-document.body.appendChild(newParagraph);
+let myList = []
+
+function AddItemButton()
+{
+    myList.push(document.getElementById('toDoItem').value)
+    let myIndex = myList.length -1;
+    const newParagraph = document.createElement('button');
+    newParagraph.innerHTML = myList[myIndex];
+
+    newParagraph.addEventListener('click', () => {
+                    Delete(index);
+    });
+    document.getElementById('todolist').appendChild(newParagraph);
+
+    console.log(myList);
+}
+
+function Delete(index)
+{
+    myList.splice(index, 1);
+
+    console.log(myList);
+}
+
